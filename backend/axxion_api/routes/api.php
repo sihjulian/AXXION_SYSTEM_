0af9d7e21912849930809bdceff6cc29b5a8f2d7
Cristian6006 <ccifuentesgaona@gmail.com>
2025-08-30 19:04:45 -0500

    Implementar controlador de roles y rutas API para gestión de roles

--- backend/axxion_api/routes/api.php
+++ backend/axxion_api/routes/api.php
@@ -1,10 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UsuarioController;
+use app\Http\Controllers\Api\UsuarioController;
+use App\Http\Controllers\Api\rolController;
 
+// USUARIOS
 
-Route::get('/usuarios', [\App\Http\Controllers\Api\UsuarioController::class, 'index']);
-Route::get('/usuario/{id}', [\App\Http\Controllers\Api\UsuarioController::class, 'show']);
-Route::delete('/usuario/{id}', [\App\Http\Controllers\Api\UsuarioController::class, 'destroy']);
-Route::post('/usuarios', [\App\Http\Controllers\Api\UsuarioController::class, 'store']);
+Route::get('/usuarios', [UsuarioController::class, 'index']);
+Route::get('/usuario/{id}', [UsuarioController::class, 'show']);
+Route::delete('/usuario/{id}', [UsuarioController::class, 'destroy']);
+Route::post('/usuarios', [UsuarioController::class, 'store']);
+
+// ROLES
+
+Route::get('/rol', [rolController::class, 'index']); 
+Route::post('/rol', [rolController::class, 'store']); 
+Route::get('/rol/{id}', [rolController::class, 'show']);
+Route::delete('/rol/{id}', [rolController::class, 'destroy']);
+Route::put('/rol/{id}', [rolController::class, 'update']);
+Route::patch('/rol/{id}', [rolController::class, 'updatePartial']);
